54313c63e5d1f3a0b4b7640b6e085f0df1b0434e
Way-Yuhao <liu697@wisc.edu>
2021-03-11 17:21:13 -0600

    added idea_simulator.py file

---
+++ simulators/idea_simulator.py
@@ -0,0 +1,7 @@
+"""
+This module generates a scaled version of the ground truth dataset without adding any noise
+"""
+
+import cv2
+import numpy as np
+from simulators.radiance_writer import radiance_writer
