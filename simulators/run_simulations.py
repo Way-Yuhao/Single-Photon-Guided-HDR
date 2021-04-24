1902b9c2027ada82c7842b46c7f40300393ec7a1
Way-Yuhao <liu697@wisc.edu>
2021-04-24 18:59:04 -0500

    before adv downsampling

--- simulators/run_simulations.py
+++ simulators/run_simulations.py
@@ -17,8 +17,8 @@ plt_path = "../simulated_outputs/plt/"
 
 """SPAD parameters"""
 SPAD_Sim = None
-SPAD_on = False             # toggle on to enable SPAD simulator
-SPAD_mono = True          # if the sensor is monochromatic
+SPAD_on = True             # toggle on to enable SPAD simulator
+SPAD_mono = False          # if the sensor is monochromatic
 SPAD_T = .01               # exposure time in seconds
 SPAD_gain = 10             # uniform gain applied to the analog signal
 SPAD_tau = 150e-9          # dead time in seconds
@@ -27,7 +27,7 @@ SPAD_qe = .4               # quantum efficiency index
 
 """CMOS parameters"""
 CMOS_Sim = None
-CMOS_on = True              # toggle on to enable CMOS simulator
+CMOS_on = False              # toggle on to enable CMOS simulator
 CMOS_mono = False           # if the sensor is monochromatic
 CMOS_fwc = 2**12            # full well capacity with a 12 bit sensor
 # CMOS_T = .01                # exposure time in seconds
